8e38198b729c4a488961e2ac73cb86566c075a3e
ugur yılmaz <ugurum212@gmail.com>
2021-10-31 22:50:05 +0300

    Add header and app details

--- src/App.js
+++ src/App.js
@@ -1,23 +1,108 @@
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
+import Header from './components/header/Header';
+import { cities } from './data/countries';
 import './App.css';
+const cityTimezones = require('city-timezones');
 
 function App() {
+
+  const [time, setTime] = useState();
+  const [timeTouched, setTimeTouched] = useState(false);
+  const [selectedCities, setSelectedCities] = useState([]);
+  const [availableCities, setAvailableCities] = useState([]);
+  const [cityFocused, setCityFocused] = useState(false);
+  const cityRef = useRef();
+
+  useEffect(() => {
+    setAvailableCities(cities);
+  }, []);
+
+  const addCity = () => {
+
+    if (!time) {
+      setTimeTouched(true);
+      return;
+    }
+    const city = cityRef.current.value;
+    // cityTimeZones -  looking up timezones
+    const cityLookup = cityTimezones.lookupViaCity(city);
+
+    if (cityLookup.length) {
+      setSelectedCities([...selectedCities, { name: city, timezone: cityLookup[0].timezone }]);
+      cityRef.current.value = '';
+      setAvailableCities(availableCities.filter(item => item !== city));
+    }
+  }
+
+  const deleteCity = (city) => {
+    setSelectedCities(selectedCities.filter(item => item.name !== city));
+    setAvailableCities([...availableCities, city]);
+  }
+
+  const citySelected = (city) => {
+    cityRef.current.value = city;
+    setCityFocused(false);
+  }
+
+  // Calculates time from given timezone
+  const timeOfCity = (tz) => {
+    const d = new Date();
+    const enteredTime = new Date(d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate() + ' ' + time);
+    return enteredTime.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', timeZone: tz });
+  }
+
+  const filterCities = (val) => {
+    const tempCities = cities.filter(city => selectedCities.map(item => item.name).indexOf(city) === -1);
+    setAvailableCities(tempCities.filter(city => city.toLocaleLowerCase().indexOf(val.toLocaleLowerCase()) !== -1));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <section className="content">
+        <div className="form-input">
+          <label>Enter Time : </label>
+          <input type="time" placeholder="--:--" onChange={(e) => setTime(e.target.value)} onBlur={() => setTimeTouched(true)} />
+          <span className="err" hidden={time || !timeTouched}>Enter valid time</span>
+        </div>
+
+        <table className="city-tbl">
+          <thead>
+            <tr>
+              <td>City</td>
+              <td>Time</td>
+              <td>TimeZone</td>
+              <td></td>
+            </tr>
+          </thead>
+          <tbody>
+            {selectedCities.map((city, index) => (
+              <tr key={index}>
+                <td>{city.name}</td>
+                <td>{timeOfCity(city.timezone)}</td>
+                <td>{city.timezone}</td>
+                <td><img src="assets/images/delete.svg" alt="delete" onClick={() => deleteCity(city.name)} /></td>
+              </tr>
+            ))
+            }
+          </tbody>
+        </table>
+
+        {!selectedCities.length && <div className="no-city">No city is selected</div>}
+
+        <div className="form-input add-city">
+          <input type="text" placeholder="Select City" ref={cityRef}
+            onChange={(e) => filterCities(e.target.value)}
+            onFocus={() => setCityFocused(true)}
+            onBlur={() => setCityFocused(false)} />
+
+          {cityFocused && <div className="city-select-list">
+            {availableCities.map((city, index) => (<span key={index} onMouseDown={() => citySelected(city)}>{city}</span>))}
+          </div>
+          }
+          <button onClick={addCity}>Add</button>
+        </div>
+      </section>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,42 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Enter Time/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('Time must be set before add action.', async () => {
+  render(<App />);
+  const errMessage = screen.getByText(/Enter valid time/i);
+  expect(errMessage).not.toBeVisible();
+
+  await userEvent.click(screen.getByText('Add'));
+  expect(errMessage).toBeVisible();
+
+  await fireEvent.change(screen.getByPlaceholderText('--:--'), {
+    target: { value: '12:12' },
+  });
+  expect(errMessage).not.toBeVisible();
+});
+
+test('Add and delete function must be add and remove Berlin', async () => {
+
+  render(<App />);
+  await fireEvent.change(screen.getByPlaceholderText('--:--'), {
+    target: { value: '08:15' },
+  });
+  await fireEvent.change(screen.getByPlaceholderText('Select City'), {
+    target: { value: 'Berlin' },
+  });
+  await userEvent.click(screen.getByText('Add'));
+  let items = await screen.findAllByText('Europe/Berlin');
+  expect(items).toHaveLength(1);
+
+  await userEvent.click(screen.queryByAltText('delete'));
+  items = await screen.findAllByText('No city is selected');
+  expect(items).toHaveLength(1);
+
 });
