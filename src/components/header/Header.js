7a88519041b56ccbabe8e8d62b077cdedfdcf426
ugur yılmaz <ugurum212@gmail.com>
2021-10-31 22:50:46 +0300

    Add assets

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Header.css";
+
+function Header() {
+
+    return <header>
+        <div>
+            <img src="./assets/images/logo.svg" alt="Logo" />
+            Time Localyzer
+        </div>
+    </header>
+}
+
+export default Header;
---
+++ src/data/countries.js
@@ -0,0 +1,1 @@
+export const cities = ['Manchester', 'Moscow', 'Amsterdam', 'Kabul', 'Brasilia', 'Berlin', 'Hamburg', 'New York', 'Tokyo', 'Istanbul', 'Islamabad', 'Odessa'];
